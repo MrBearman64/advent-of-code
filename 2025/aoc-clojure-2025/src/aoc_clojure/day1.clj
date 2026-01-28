c7c427fc07bf09e1afe7f50f768aeb34b6bde385
Alexander Adrio <aadrio@ai-ag.de>
2026-01-28 17:07:26 +0100

    Solve day1 part 1

--- 2025/aoc-clojure-2025/src/aoc_clojure/day1.clj
+++ 2025/aoc-clojure-2025/src/aoc_clojure/day1.clj
@@ -1,15 +1,28 @@
-(ns aoc-clojure.day1)
+(ns aoc-clojure.day1
+  (:require
+   [clojure.java.io :as io]))
+(require '[clojure.string :as str]
+         '[clojure.java.io :as io])
+
+(defn readChallengeInput
+  []
+  (with-open [rdr (io/reader ".\\resources\\day1\\input.txt")]
+    (->> rdr
+         .lines
+         .toList
+         (map str/trim)
+         (filter #(not (empty? %))))))
 
 (defn solveDay1Part1
-  ([_ acc]
-  acc)
-  ([pos acc [[dir & rot] & rotations]]
-   (println dir rot)
-   (println (class dir))
-  (let [
-        newPos (case dir
-                \L (+ pos rot)
-                \R (- pos rot))
-        newAcc (if (zero? newPos) (inc acc) acc)
-        ]
-    (recur newPos newAcc rotations))))
+  ([pos acc [rotation & rotations]]
+   (println rotation)
+   (if (nil? rotation) acc
+       (let [[direction & p2] rotation
+             rot (Integer/parseInt (str/join p2))
+             newPos (mod (case direction
+                           \L (+ pos rot)
+                           \R (- pos rot)) 100)
+             newAcc (if (zero? newPos) (inc acc) acc)]
+
+         (printf "rot %s, dir: %s, rot: %s, newPos: %s, newAcc: %s" rotation direction rot newPos newAcc)
+         (recur newPos newAcc rotations)))))
--- 2025/aoc-clojure-2025/test/aoc_clojure/day1_test.clj
+++ 2025/aoc-clojure-2025/test/aoc_clojure/day1_test.clj
@@ -2,18 +2,14 @@
   (:require [clojure.test :refer :all]
             [aoc-clojure.day1 :refer :all]))
 
-(deftest a-test
+(def rotations ["L68" "L30" "R48" "L5" "R60" "L55" "L1" "L99" "R14" "L82"])
+
+(deftest example-test
   (testing "Day 1 - Part 1"
-    (let [rotations [
-                     "L68"
-                     "L30"
-                     "R48"
-                     "L5"
-                     "R60"
-                     "L55"
-                     "L1"
-                     "L99"
-                     "R14"
-                     "L82"
-                     ]]
-    (is (= (solveDay1Part1 0 50 rotations) 3)))))
+    (is (= (solveDay1Part1 50 0 rotations) 3))))
+
+(deftest day1-file-test
+  (testing "Day 1 - Part 1 - File Input"
+    (let [input (seq (readChallengeInput))]
+      (is (#(not (zero? %))) (solveDay1Part1 50 0 input)))))
+
