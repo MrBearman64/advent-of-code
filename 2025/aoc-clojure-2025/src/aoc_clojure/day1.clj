4c57f2af96eb1f2f0e222769f0c66064dd7481d9
Alexander Adrio <alexander.adrio@gmx.de>
2026-01-28 22:16:12 +0100

    fix(clj-2025-day1): more idiomatic clojure

--- 2025/aoc-clojure-2025/src/aoc_clojure/day1.clj
+++ 2025/aoc-clojure-2025/src/aoc_clojure/day1.clj
@@ -1,28 +1,39 @@
 (ns aoc-clojure.day1
   (:require
-   [clojure.java.io :as io]))
-(require '[clojure.string :as str]
-         '[clojure.java.io :as io])
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
 
-(defn readChallengeInput
+(defn read-challenge-input
   []
-  (with-open [rdr (io/reader ".\\resources\\day1\\input.txt")]
-    (->> rdr
-         .lines
-         .toList
-         (map str/trim)
-         (filter #(not (empty? %))))))
+  (->> (io/resource "day1/input.txt")
+       io/reader
+       line-seq
+       (map str/trim)
+       (remove str/blank?)))
 
-(defn solveDay1Part1
+(defn solve-day1-part1
   ([pos acc [rotation & rotations]]
-   (println rotation)
-   (if (nil? rotation) acc
-       (let [[direction & p2] rotation
-             rot (Integer/parseInt (str/join p2))
-             newPos (mod (case direction
-                           \L (+ pos rot)
-                           \R (- pos rot)) 100)
-             newAcc (if (zero? newPos) (inc acc) acc)]
+   ;;(println rotation)
+   (if (str/blank? rotation)
+     (do
+       (printf "rot %s, newPos: %s, newAcc: %s%n" rotation pos acc)
+       acc)
+     (let [[direction & p2] rotation
+           rot (Integer/parseInt (str/join p2))
+           newPos (mod (case direction
+                         \L (+ pos rot)
+                         \R (- pos rot)) 100)
+           newAcc (if (zero? newPos) (inc acc) acc)]
+       (recur newPos newAcc rotations)))))
 
-         (printf "rot %s, dir: %s, rot: %s, newPos: %s, newAcc: %s" rotation direction rot newPos newAcc)
-         (recur newPos newAcc rotations)))))
+;; Style critique by daddy GPT
+
+(defn- step [{:keys [pos acc]} rotation]
+  (let [dir (first rotation)
+        amount (parse-long (apply str (subs rotation 1)))
+        pos' (mod (({\L + \R -} dir) pos amount) 100)
+        acc' (if (zero? pos') (inc acc) acc)]
+    {:pos pos' :acc acc'}))
+
+(defn solve-day1-part1-functional [pos rotations]
+  (:acc (reduce step {:pos pos :acc 0} rotations)))
--- 2025/aoc-clojure-2025/test/aoc_clojure/day1_test.clj
+++ 2025/aoc-clojure-2025/test/aoc_clojure/day1_test.clj
@@ -1,15 +1,21 @@
 (ns aoc-clojure.day1-test
-  (:require [clojure.test :refer :all]
-            [aoc-clojure.day1 :refer :all]))
+  (:require
+   [aoc-clojure.day1 :refer [read-challenge-input solve-day1-part1
+                             solve-day1-part1-functional]]
+   [clojure.test :refer [deftest is testing]]))
 
 (def rotations ["L68" "L30" "R48" "L5" "R60" "L55" "L1" "L99" "R14" "L82"])
 
 (deftest example-test
   (testing "Day 1 - Part 1"
-    (is (= (solveDay1Part1 50 0 rotations) 3))))
+    (is (= (solve-day1-part1 50 0 rotations) 3))))
+
+(deftest example-test-functional
+  (testing "Day 1 - Part 1"
+    (is (= (solve-day1-part1-functional 50 rotations) 3))))
 
 (deftest day1-file-test
   (testing "Day 1 - Part 1 - File Input"
-    (let [input (seq (readChallengeInput))]
-      (is (#(not (zero? %))) (solveDay1Part1 50 0 input)))))
+    (let [input (seq (read-challenge-input))]
+      (is ((complement zero?) (solve-day1-part1 50 0 input))))))
 
